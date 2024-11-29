ae3677d9b11c9f5a1037c0dc64331beaa17b6a3c
CheepeerDev <lucasribeiror011@gmail.com>
2024-11-29 11:20:50 -0300

    Adicionar arquivos de projeto.

---
+++ Jogo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Launcher
+{
+    class Jogo
+    {
+        int id;
+        String nome;
+        Boolean favorito;
+        String caminho;
+        String icone;
+    }
+}
